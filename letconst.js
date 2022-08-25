ca157c7e6b90ed6f132e08335a7a9ae426743b6c
Sadia131 <sadiaobaed6969@gmail.com>
2022-08-25 15:37:17 +0600

    done

---
+++ letconst.js
@@ -0,0 +1,18 @@
+const myName = "Sadia";
+console.log(myName); 
+
+const numbers = [11, 33, 445]
+numbers[1]= 23;
+numbers.push(32);
+//numbers= ["asha", "aswa", "ssd"]
+//const hero = {name: "sadkib", phone: 01234};
+console.log(numbers);
+
+let patientName = "dipro";
+patientName = "khukumoni";
+console.log(patientName);
+let sum = 0;
+for(let i=0; i< 10; i++){
+    sum = sum + 1;
+}
+console.log(i);
